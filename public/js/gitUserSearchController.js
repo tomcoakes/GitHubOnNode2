ce2505180c57af04f783d779dbe557516e18ccd3
Tom Coakes <tomcoakes@gmail.com>
2015-03-20 09:05:42 +0000

    doSearch function is run on click and generates hardcoded JSON

---
+++ public/js/gitUserSearchController.js
@@ -0,0 +1,23 @@
+githubUserSearch.controller('GitUserSearchController', function($scope) {
+
+
+
+$scope.doSearch = function() {
+  $scope.searchResult = {
+    "items": [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+      }, 
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ]
+  };
+};
+
+
+});
---
+++ tests/gitUserSearchController.spec.js
@@ -0,0 +1,37 @@
+describe('GitUserSearchController', function() {
+  beforeEach(module('GitUserSearch'));
+
+  var scope, ctrl;
+
+  beforeEach(inject(function($rootScope, $controller) {
+    scope = $rootScope.$new();
+    ctrl = $controller('GitUserSearchController', {
+        $scope: scope
+    });
+  }));
+
+  it('initialises with an empty search result and term', function() {
+    expect(scope.searchResult).toBeDefined();
+    expect(scope.searchTerm).toBeUndefined();
+  });
+
+  describe('when searching for a user', function() {
+    var items = [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+      }, 
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ];
+
+    it('displays search results', function() {
+      expect(scope.searchResult.items).toEqual(items);
+    });
+  });
+
+});
